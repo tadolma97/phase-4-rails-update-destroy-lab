e1f12e2c866cc03d83fa5c83d849ec02533bf717
Tashi Dolma <tadolma97@gmail.com>
2022-06-28 11:14:40 -0400

    commit tree e132102e6121252691b604e378a0f3b68a522873

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = Plant.find_by(id: params[:id])
+    
+    if plant 
+      plant.update(plant_params)
+      render json: plant 
+    else 
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  def plant_params
+    params.permit(:id, :name, :image, :price, :is_in_stock)
+  end
+
+  def destroy 
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+
+  end
+
   private
 
   def plant_params
